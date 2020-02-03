f9c3ef741d4ce5199c37691e33e338b475ba3b24
alexeyraga <alexey.raga@gmail.com>
2020-02-03 15:33:55 +1100

    Add ToEncoding class and instances

--- src/Data/Avro/Encode.hs
+++ src/Data/Avro/Encode.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE RecordWildCards      #-}
 {-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeApplications     #-}
 {-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
@@ -165,12 +166,143 @@ getSchema :: forall a. EncodeAvro a => a -> Schema
 getSchema = snd . runAvro . avro
 
 getType :: EncodeAvro a => Proxy a -> Schema
-getType = getSchema . (asProxyTypeOf undefined)
+getType = getSchema . asProxyTypeOf undefined
 -- N.B. ^^^ Local knowledge that 'fst' won't be used,
 -- so the bottom of 'undefined' will not escape so long as schema creation
 -- remains lazy in the argument.
 
 newtype AvroM = AvroM { runAvro :: (Builder, Schema) }
+
+-- class HasSchema a where
+--   aSchema :: Schema
+
+class ToEncoding a where
+  toEncoding :: Schema -> a -> Builder
+
+instance ToEncoding Int where
+  toEncoding S.Long i = encodeRaw @Int64 (fromIntegral i)
+  toEncoding S.Int i  = encodeRaw @Int32 (fromIntegral i)
+  toEncoding s _      = error ("Unable to encode Int as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding Int32 where
+  toEncoding S.Long i = encodeRaw @Int64 (fromIntegral i)
+  toEncoding S.Int i  = encodeRaw @Int32 i
+  toEncoding s _      = error ("Unable to encode Int32 as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding Int64 where
+  toEncoding S.Long i = encodeRaw @Int64 i
+  toEncoding s _      = error ("Unable to encode Int64 as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding Word8 where
+  toEncoding S.Int i  = encodeRaw @Word8 i
+  toEncoding S.Long i = encodeRaw @Word64 (fromIntegral i)
+  toEncoding s _      = error ("Unable to encode Word8 as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding Word16 where
+  toEncoding S.Int i  = encodeRaw @Word16 i
+  toEncoding S.Long i = encodeRaw @Word64 (fromIntegral i)
+  toEncoding s _      = error ("Unable to encode Word16 as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding Word32 where
+  toEncoding S.Int i  = encodeRaw @Word32 i
+  toEncoding S.Long i = encodeRaw @Word64 (fromIntegral i)
+  toEncoding s _      = error ("Unable to encode Word32 as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding Word64 where
+  toEncoding S.Long i = encodeRaw @Word64 i
+  toEncoding s _      = error ("Unable to encode Word64 as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding Double where
+  toEncoding S.Double i = word64LE (IEEE.doubleToWord i)
+  toEncoding s _        = error ("Unable to encode Double as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding Float where
+  toEncoding S.Float i  = word32LE (IEEE.floatToWord i)
+  toEncoding S.Double i = word64LE (IEEE.doubleToWord $ realToFrac i)
+  toEncoding s _        = error ("Unable to encode Float as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding Bool where
+  toEncoding S.Boolean v = word8 $ fromIntegral (fromEnum v)
+  toEncoding s _         = error ("Unable to encode Bool as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding B.ByteString where
+  toEncoding s bs = case s of
+    S.Bytes                          -> encodeRaw (B.length bs) <> byteString bs
+    S.Fixed _ _ l | l == B.length bs -> byteString bs
+    S.Fixed _ _ l                    -> error ("Unable to encode ByteString as Fixed(" <> show l <> ") because its length is " <> show (B.length bs))
+    _                                -> error ("Unable to encode ByteString as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding BL.ByteString where
+  toEncoding s bs = toEncoding s (BL.toStrict bs)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding Text where
+  toEncoding s v =
+    let
+      bs = T.encodeUtf8 v
+      res = encodeRaw (B.length bs) <> byteString bs
+    in case s of
+      S.Bytes  -> res
+      S.String -> res
+      _        -> error ("Unable to encode Text as: " <> show s)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding TL.Text where
+  toEncoding s v = toEncoding s (TL.toStrict v)
+  {-# INLINE toEncoding #-}
+
+instance ToEncoding a => ToEncoding [a] where
+  toEncoding (S.Array s) as =
+    if DL.null as then long0 else encodeRaw (F.length as) <> foldMap (toEncoding s) as <> long0
+  toEncoding s _         = error ("Unable to encode Haskell list as: " <> show s)
+
+instance ToEncoding a => ToEncoding (V.Vector a) where
+  toEncoding (S.Array s) as =
+    if V.null as then long0 else encodeRaw (V.length as) <> foldMap (toEncoding s) as <> long0
+  toEncoding s _         = error ("Unable to encode Vector list as: " <> show s)
+
+instance (Ix i, ToEncoding a) => ToEncoding (Ar.Array i a) where
+  toEncoding (S.Array s) as =
+    if F.length as == 0 then long0 else encodeRaw (F.length as) <> foldMap (toEncoding s) as <> long0
+  toEncoding s _         = error ("Unable to encode indexed Array list as: " <> show s)
+
+instance (U.Unbox a, ToEncoding a) => ToEncoding (U.Vector a) where
+  toEncoding (S.Array s) as =
+    if U.null as then long0 else encodeRaw (U.length as) <> foldMap (toEncoding s) (U.toList as) <> long0
+  toEncoding s _         = error ("Unable to encode Vector list as: " <> show s)
+
+instance ToEncoding a => ToEncoding (HashMap Text a) where
+  toEncoding (S.Map s) hm =
+    if HashMap.null hm then long0 else putI (F.length hm) <> foldMap putKV (HashMap.toList hm) <> long0
+    where putKV (k,v) = toEncoding S.String k <> toEncoding s v
+  toEncoding s _         = error ("Unable to encode HashMap as: " <> show s)
+
+instance ToEncoding a => ToEncoding (Maybe a) where
+  toEncoding (S.Union opts) v =
+    case V.toList opts of
+      [S.Null, s] -> maybe (putI 0) (\a -> putI 1 <> toEncoding s a) v
+      wrongOpts   -> error ("Unable to encode Maybe as " <> show wrongOpts)
+  toEncoding s _ = error ("Unable to encode Maybe as " <> show s)
+
+instance (ToEncoding a, ToEncoding b) => ToEncoding (Either a b) where
+  toEncoding (S.Union opts) v =
+    case V.toList opts of
+      [sa, sb] -> case v of
+        Left a  -> putI 0 <> toEncoding sa a
+        Right b -> putI 1 <> toEncoding sb b
+      wrongOpts   -> error ("Unable to encode Either as " <> show wrongOpts)
+  toEncoding s _ = error ("Unable to encode Either as " <> show s)
 
 class EncodeAvro a where
   avro :: a -> AvroM
@@ -184,6 +316,7 @@ avroLong n = AvroM (encodeRaw n, S.Long)
 -- Put a Haskell Int.
 putI :: Int -> Builder
 putI = encodeRaw
+{-# INLINE putI #-}
 
 instance EncodeAvro Int  where
   avro = avroInt
@@ -230,6 +363,7 @@ instance EncodeAvro Float where
 -- Terminating word for array and map types.
 long0 :: Builder
 long0 = encodeRaw (0 :: Word64)
+{-# INLINE long0 #-}
 
 instance EncodeAvro a => EncodeAvro [a] where
   avro a = AvroM ( if DL.null a then long0 else encodeRaw (F.length a) <> foldMap putAvro a <> long0
@@ -265,8 +399,15 @@ instance EncodeAvro a => EncodeAvro (HashMap Text a) where
 
 -- | Maybe is modeled as a sum type `{null, a}`.
 instance EncodeAvro a => EncodeAvro (Maybe a) where
-  avro Nothing  = AvroM (putI 0             , S.mkUnion (S.Null:|[S.Int]))
-  avro (Just x) = AvroM (putI 1 <> putAvro x, S.mkUnion (S.Null:|[S.Int]))
+  avro Nothing  = AvroM (putI 0             , S.mkUnion (S.Null:|[getType (Proxy :: Proxy a)]))
+  avro (Just x) = AvroM (putI 1 <> putAvro x, S.mkUnion (S.Null:|[getType (Proxy :: Proxy a)]))
+
+instance (EncodeAvro a, EncodeAvro b) => EncodeAvro (Either a b) where
+  avro v =
+    let unionType = S.mkUnion (getType (Proxy :: Proxy a):|[getType (Proxy :: Proxy b)])
+    in case v of
+      Left a  -> AvroM (putI 0 <> putAvro a, unionType)
+      Right b -> AvroM (putI 1 <> putAvro b, unionType)
 
 instance EncodeAvro () where
   avro () = AvroM (mempty, S.Null)
